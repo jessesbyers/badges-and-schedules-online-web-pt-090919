0de467600c8b360cd4cca41728de1bc522eb5cf7
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-11 13:26:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,26 +5,21 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  new_array = []
+  new_array1 = []
   array.each do |name|
-    new_array << "Hello, my name is #{name}."
+    new_array1 << "Hello, my name is #{name}."
   end
-  new_array
+  new_array1
 end
 
 def assign_rooms(array)
-  new_array = []
+  new_array2 = []
   array.each_with_index do |name, index|
-    new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    new_array2 << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
-  new_array
+  new_array2
 end
 
-def printer(batch_badge_creator, assign_rooms)
-  batch_badge_creator.each do |badge|
-    puts badge
-  assign_rooms.each do |room_assignment|
-    puts room_assignment
-  end
-  end
+def printer(bat)
+
 end
