32ba098f6e205c085a7783b7e9dfa12ad48296fc
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-11 14:25:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,6 @@ def assign_rooms(array)
 end
 
 def printer(attendees)
-    batch_badge_creator(attendees).each do |badge|
-      puts badge
-    assign_rooms(attendees).each do |room|
-      puts room
-    end
-end
+    puts batch_badge_creator(attendees)
+    puts assign_rooms(attendees)
 end
